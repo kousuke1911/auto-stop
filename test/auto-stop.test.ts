6dce239aa28dc88914b79b82797b100a8852edc6
kousuke.michihata@gmail.com <kousuke1911>
2025-02-14 14:01:29 +0000

    プロジェクトだけ

---
+++ test/auto-stop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutoStop from '../lib/auto-stop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auto-stop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutoStop.AutoStopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
